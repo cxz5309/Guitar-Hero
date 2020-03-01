e2c721598c02468c6a667393c9f04b4d325023f2
cxz5309 <cxz5309@gmail.com>
2020-03-01 15:58:37 +0900

    3 commit

--- Guitar Hero/Assets/Activator.cs
+++ Guitar Hero/Assets/Activator.cs
@@ -7,9 +7,12 @@ public class Activator : MonoBehaviour
     SpriteRenderer sr;
     public KeyCode key;
     bool active = false;
-    GameObject note;
+    GameObject note, gm;
     Color old;
+    public bool CreateNote;
+    public GameObject n;
 
+    
     // Start is called before the first frame update
     private void Awake()
     {
@@ -18,27 +21,43 @@ public class Activator : MonoBehaviour
 
     void Start()
     {
+        gm = GameObject.Find("GameManager");
         old = sr.color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(key))
-        {
-            StartCoroutine(Pressed());
+        if(CreateNote){
+            if(Input.GetKeyDown(key))
+            Instantiate(n, transform.position, Quaternion.identity);
         }
+        else {
+            if (Input.GetKeyDown(key))
+            {
+                StartCoroutine(Pressed());
+            }
 
-        if (Input.GetKeyDown(key) && active)
-        {
-            Destroy(note);
+            if (Input.GetKeyDown(key) && active)
+            {
+                Destroy(note);
+                AddScore();
+                gm.GetComponent<GameManager>().AddStreak();
+                active = false;
+            }
+            else if (Input.GetKeyDown(key) && !active)
+            {
+                gm.GetComponent<GameManager>().ResetStreak();
+            }
         }
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         active = true;
-        if(collision.gameObject.tag =="Note")
+
+        if (collision.gameObject.tag =="Note")
         {
             note = collision.gameObject;
         }
@@ -47,7 +66,15 @@ public class Activator : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         active = false;
+        //gm.GetComponent<GameManager>().ResetStreak();
     }
+
+    void AddScore()
+    {
+        PlayerPrefs.SetInt("Score", PlayerPrefs.GetInt("Score") + gm.GetComponent<GameManager>().GetScore());
+    }
+
+
 
     IEnumerator Pressed()
     {
