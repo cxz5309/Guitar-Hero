665a06360d355a5d902f628e76e09b6ef4f00087
cxz5309 <cxz5309@gmail.com>
2020-03-01 13:22:02 +0900

    2 commit

--- Guitar Hero/Assets/Activator.cs
+++ Guitar Hero/Assets/Activator.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class Activator : MonoBehaviour
 {
+    SpriteRenderer sr;
     public KeyCode key;
     bool active = false;
     GameObject note;
+    Color old;
 
     // Start is called before the first frame update
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
     void Start()
     {
-        
+        old = sr.color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(Input.GetKeyDown(key) && active)
+        if (Input.GetKeyDown(key))
+        {
+            StartCoroutine(Pressed());
+        }
+
+        if (Input.GetKeyDown(key) && active)
         {
             Destroy(note);
         }
@@ -36,5 +47,12 @@ public class Activator : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         active = false;
+    }
+
+    IEnumerator Pressed()
+    {
+        sr.color = new Color(0, 0, 0);
+        yield return new WaitForSeconds(0.05f);
+        sr.color = old;
     }
 }
